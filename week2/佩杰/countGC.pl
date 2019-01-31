1655b7f1a31f6cd5556f05d271dc678e227e5fed
Echo <ysy16816888@gmail.com>
2019-01-31 21:38:53 +0800

    add perl solution

---
+++ week2/佩杰/countGC.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+open SEQ, "< :encoding(utf-8)", "./chr1.fa" || die "Cannot open file!";
+
+my $numC = 0;
+my $numG = 0;
+my $numA = 0;
+my $numT = 0;
+
+<SEQ>;
+while (my $line = <SEQ>) {
+	$numA += $line =~ s/A/A/g;
+	$numT += $line =~ s/T/T/g;
+	$numC += $line =~ s/C/C/g;
+	$numG += $line =~ s/G/G/g;
+}
+
+my $CGnum = ($numC + $numG) / ($numG + $numC + $numT + $numA);
+print $CGnum;
